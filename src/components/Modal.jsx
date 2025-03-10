14e71dceb62a756ce22c5317ad97d80e67878cec
ethry <58019814+ethrythedev@users.noreply.github.com>
2025-03-11 00:28:36 +1300

    note editing modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,97 @@
+import { useState, useRef, useEffect } from 'preact/hooks';
+
+const Modal = ({ isOpen, closeModal, children }) => {
+  const modalRef = useRef();
+  const focusableElementsRef = useRef([]);
+  const [isClosing, setIsClosing] = useState(false); 
+
+  const getFocusableElements = () => {
+    return modalRef.current
+      ? Array.from(modalRef.current.querySelectorAll('button, a, input, select, textarea, [tabindex]:not([tabindex="-1"])'))
+      : [];
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        handleClose(); // close modal if clicked outside
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isOpen]);
+
+  // focus trap
+  useEffect(() => {
+    if (isOpen) {
+      focusableElementsRef.current = getFocusableElements();
+      if (focusableElementsRef.current.length > 0) {
+        focusableElementsRef.current[0].focus();
+      }
+    }
+  }, [isOpen]);
+
+  const handleClose = () => {
+    setIsClosing(true); 
+
+    setTimeout(() => { 
+        setIsClosing(false);
+    }, 300);
+
+    setTimeout(() => {
+        closeModal();
+    }, 50);
+  };
+
+  const handleKeyDown = (event) => {
+    const { key, shiftKey } = event;
+
+    if (key === 'Tab' && focusableElementsRef.current.length > 0) {
+      const focusable = focusableElementsRef.current;
+      const currentIndex = focusable.indexOf(document.activeElement);
+
+      if (shiftKey) {
+        if (currentIndex === 0) {
+          focusable[focusable.length - 1].focus();
+          event.preventDefault();
+        }
+      } else {
+        if (currentIndex === focusable.length - 1) {
+          focusable[0].focus();
+          event.preventDefault();
+        }
+      }
+    }
+  };
+
+  return isOpen || isClosing ? (
+    <>
+      <div
+        className={`modal-overlay ${isOpen ? '' : 'closing'}`}
+        onClick={handleClose}
+      />
+
+      <div
+        ref={modalRef}
+        role="dialog"
+        aria-labelledby="modal-title"
+        aria-hidden={!isOpen}
+        tabIndex={-1}
+        onKeyDown={handleKeyDown}
+        className={`modal ${isOpen ? '' : 'closing'}`}
+      >
+        <div className="modal-content">
+            <button className="modal-close-btn" tabIndex={0} onClick={(e) => { handleClose() }}>
+                <i class="bi bi-x-lg"></i>
+            </button>
+            {children}
+        </div>
+      </div>
+    </>
+  ) : null;
+};
+
+export default Modal;
--- src/components/NoteGrid.jsx
+++ src/components/NoteGrid.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from 'preact/hooks';
 import Cookies from 'universal-cookie';
 import wretch from 'wretch';
+import Modal from './Modal';
 
 export default function NoteGrid() {
     const [notes, setNotes] = useState([]);
+    const [noteModalContents, setNoteModalContents] = useState({title:"loading", content: "loading"});
+
+    const [noteModalEditedToTitle, setNoteModalEditedToTitle] = useState("");
+    const [noteModalEditedToContent, setNoteModalEditedToContent] = useState("");
+
+    const [modalTipClass, setModalTipClass] = useState("text-danger");
+    const [modalTip, setModalTip] = useState("Error");
 
     const cookies = new Cookies(null, { path: "/" });
 
-    useEffect(() => {
+    const refreshNotes = () => {
         wretch(`${cookies.get("noteauth_hostname")}/notes`)
             .auth(`Bearer ${cookies.get("noteauth_token")}`)
             .get()
@@ -16,16 +24,87 @@ export default function NoteGrid() {
                     setNotes(resp);
                 }
             });
+    }
+
+    useEffect(() => {
+        refreshNotes();
     }, []);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const modalSet = (id) => {
+        // TODO: update live w/ websockets
+        wretch(`${cookies.get("noteauth_hostname")}/notes/${id}`)
+            .auth(`Bearer ${cookies.get("noteauth_token")}`)
+            .get()
+            .json((resp) => {
+                setNoteModalContents(resp);
+
+                setModalTipClass("text-danger");
+                setModalTip("");
+
+                setNoteModalEditedToTitle(resp.title);
+                setNoteModalEditedToContent(resp.content);
+
+                // open modal only once loaded
+                openModal();
+            });
+    }
+
+    const updateNote = (e) => {
+        e.preventDefault();
+    
+        const cookies = new Cookies(null, { path: '/' });
+    
+        wretch(`${cookies.get('noteauth_hostname')}/notes/${noteModalContents.id}`)
+          .auth(`Bearer ${cookies.get('noteauth_token')}`)
+          .put({ title: noteModalEditedToTitle, content: noteModalEditedToContent })
+          .res(async (response) => { 
+            const resp = await response.json();
+            if(resp.success && resp.success === true) {
+                setModalTipClass("text-success");
+                setModalTip("Saved!");
+            }
+          })
+          .catch(async (error) => {
+            setModalTipClass("text-danger");
+            setModalTip("Error, check console");
+
+            console.error(error);
+          });
+
+        setTimeout(() => {
+            refreshNotes();
+        }, 100);
+    }
+
     return (
-        <div className="note-grid">
-            {notes.map((note) => (
-                <div className="note-grid-item" key={note.id}>
-                    <h2>{note.title}</h2>
-                    <p>{note.content}</p>
-                </div>
-            ))}
-        </div>
+        <section>
+            <Modal isOpen={isModalOpen} closeModal={closeModal}>
+                <form onSubmit={updateNote}>
+                    <h2 className="note-modal-title m-0 p-0 note-modal-input-element-either" tabIndex={0} contentEditable={true} spellCheck={true} onInput={(e) => {setNoteModalEditedToTitle(e.target.innerText)}}>{noteModalContents.title}</h2>
+                    <p className="m-0 p-0 note-modal-input-element-either" tabIndex={0} contentEditable={true} spellCheck={true} onInput={(e) => {setNoteModalEditedToContent(e.target.innerText)}}>
+                        {noteModalContents.content}
+                    </p>
+
+                    <div className="note-modal-buttons-end">
+                        <span className={modalTipClass}>{modalTip}</span>
+                        <button className="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </Modal>
+
+            <div className="note-grid">
+                {notes.map((note) => (
+                    <div className="note-grid-item" onClick={(e) => {modalSet(note.id)}} key={note.id} tabIndex={0}>
+                        <h2>{note.title}</h2>
+                        <p className="note-grid-paragraph">{note.content}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
     );
 }
