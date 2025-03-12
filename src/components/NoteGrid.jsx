7c656d6f56d83d7f96d1a94c5ffb37f0298677fc
ethry <58019814+ethrythedev@users.noreply.github.com>
2025-03-12 23:23:07 +1300

    notes are now refreshed via socket on delete

--- src/components/NoteGrid.jsx
+++ src/components/NoteGrid.jsx
@@ -78,7 +78,6 @@ export default function NoteGrid() {
             .delete()
             .json((resp) => {
                 closeModal();
-                refreshNotes();
 
                 setSecondDeleteModalOpened(false);
             });
