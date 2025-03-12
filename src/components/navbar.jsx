23093d8edbb40e387830c8077bf68a9861f24cdd
ethry <58019814+ethrythedev@users.noreply.github.com>
2025-03-12 20:58:41 +1300

    you're online/offline indicator

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'preact/hooks'
 import "@fontsource/inter";
 import Link from './Link';
+import { Toaster } from 'react-hot-toast';
 
 export default function NavBar(props) {
   const [count, setCount] = useState(0)
 
   return (
     <nav>
-        <img src="/logo.svg" alt="Note logo" className="logo" onDragStart={(e) => {e.preventDefault(); return false;}} onContextMenu={(e) => {e.preventDefault(); return false;}} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
 
-        {props.loggedIn ? 
-            <div className="nav-right-elements-loggedin">
-                <Link href="/app" className="nav-right-link">My Notes</Link>
-                <Link href="/logout" className="nav-right-link nav-right-logout-link">Logout</Link>
-            </div>
-        : ""}
+      <img src="/logo.svg" alt="Note logo" className="logo" onDragStart={(e) => {e.preventDefault(); return false;}} onContextMenu={(e) => {e.preventDefault(); return false;}} />
+
+      {props.loggedIn ? 
+          <div className="nav-right-elements-loggedin">
+              <Link href="/app" className="nav-right-link">My Notes</Link>
+              <Link href="/logout" className="nav-right-link nav-right-logout-link">Logout</Link>
+          </div>
+      : ""}
         
     </nav>
   )
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 import Cookies from "universal-cookie";
+import toast from "react-hot-toast";
+
+var haveBeenOnlineBefore = false;
 
 const cookies = new Cookies(null, { path: "/" });
 
@@ -8,8 +11,34 @@ const socket = io(cookies.get("noteauth_hostname"), {
     transports: ["websocket"],
 });
 
+let debounceConnectTimeout;
+let debounceDisconnectTimeout;
+
 socket.on("connect", () => {
-    console.log("Connected to WebSocket server");
+    // debounce
+    clearTimeout(debounceConnectTimeout);
+
+    debounceConnectTimeout = setTimeout(() => {
+        // 'you're online!' toast
+        if (haveBeenOnlineBefore) {
+            toast.success("You're back online!");
+        }
+
+        haveBeenOnlineBefore = true;
+
+        console.log("Connected to WebSocket server");
+    }, 300);
+});
+
+socket.on("disconnect", (reason) => {
+    // debounce
+    clearTimeout(debounceDisconnectTimeout);
+
+    debounceDisconnectTimeout = setTimeout(() => {
+        // 'youre offline' toast
+        console.log(`Disconnected from WebSocket server. Reason: ${reason}`);
+        toast.error("You're offline");
+    }, 300);
 });
 
 export default socket;
